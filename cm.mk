e2be86094f8162aa4174dba8495ae4b883a4ef63
MTK <MTK@MTK.com>
2016-07-12 17:45:20 -0400

    mkvendor.sh: Initial commit of 4032A

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := 4032A
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/ALCATEL/4032A/device_4032A.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := 4032A
+PRODUCT_NAME := cm_4032A
+PRODUCT_BRAND := ALCATEL
+PRODUCT_MODEL := 4032A
+PRODUCT_MANUFACTURER := ALCATEL
