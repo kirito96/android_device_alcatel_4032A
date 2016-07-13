ea4c95caa315a794ae57c63023891b3e2b479f62
carliv <carliv@maxand.ro>
2016-07-13 08:57:30 +0300

    fix build CTR 3

--- cm.mk
+++ cm.mk
@@ -4,6 +4,9 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := 4032A
 
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
