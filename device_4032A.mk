54bf8c940790a803931d737ced0ff0b6b768727e
kirito96 <kirito96@users.noreply.github.com>
2016-07-12 23:01:37 -0400

    update to build recovery

--- device_4032A.mk
+++ device_4032A.mk
@@ -7,7 +7,7 @@ $(call inherit-product-if-exists, vendor/ALCATEL/4032A/4032A-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/ALCATEL/4032A/overlay
 
-LOCAL_PATH := device/ALCATEL/4032A
+LOCAL_PATH := device/ALCATEL/4032A/prebuilt
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -22,3 +22,18 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_4032A
 PRODUCT_DEVICE := 4032A
+
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
+ro.allow.mock.location=1 \
+persist.mtk.aee.aed=on \
+ro.debuggable=1 \
+persist.service.acm.enable=0 \
+persist.sys.usb.config=mass_storage \
+ro.bootloader.mode=download \
+ro.mount.fs=EXT4 \
+ro.persist.partition.support=no
+
+PRODUCT_COPY_FILES_OVERRIDES += \
+    root/fstab.goldfish \
+    root/init.goldfish.rc \
+    recovery/root/fstab.goldfish
