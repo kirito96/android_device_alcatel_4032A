424a96c4412886781aac074d784c89054c3b56d4
kirito96 <kirito96@users.noreply.github.com>
2016-07-19 21:16:05 -0400

    update to build twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/ALCATEL/4032A/BoardConfigVendor.mk
+-include vendor/alcatel/4032A/BoardConfigVendor.mk
 
 # Architecture
 TARGET_ARCH := arm
@@ -30,13 +30,12 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_MKBOOTIMG := $(HOST_OUT_EXECUTABLES)/mtkbootimg$(HOST_EXECUTABLE_SUFFIX)
-BOARD_CUSTOM_BOOTIMG_MK := device/ALCATEL/4032A/bootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/4032A/bootimg.mk
 BOARD_MKBOOTIMG_ARGS := --mtk 1
 
-TARGET_PREBUILT_KERNEL := device/ALCATEL/4032A/prebuilt/kernel
-TARGET_RECOVERY_INITRC := device/ALCATEL/4032A/recovery/init.mt6572.rc
-TARGET_RECOVERY_FSTAB := device/ALCATEL/4032A/recovery/recovery.fstab
-
+TARGET_PREBUILT_KERNEL := device/alcatel/4032A/prebuilt/kernel
+TARGET_RECOVERY_INITRC := device/alcatel/4032A/recovery/init.rc
+TARGET_RECOVERY_FSTAB := device/alcatel/4032A/recovery/recovery.fstab
 
 DEVICE_SCREEN_WIDTH := 480
 DEVICE_SCREEN_HEIGHT := 800
@@ -44,10 +43,13 @@ DEVICE_SCREEN_HEIGHT := 800
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-RECOVERY_VARIANT := carliv
+RECOVERY_VARIANT := twrp
+#RECOVERY_VARIANT := philz
+#RECOVERY_VARIANT := cwm
+#RECOVERY_VARIANT := carliv
 
 ifneq ($(RECOVERY_VARIANT),carliv)
-TARGET_RECOVERY_FSTAB := device/ALCATEL/4032A/recovery/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/alcatel/4032A/recovery/recovery.fstab
 endif
 
 
@@ -61,14 +63,29 @@ endif
 
 # PHILZ
 ifeq ($(RECOVERY_VARIANT),philz)
-TARGET_COMMON_NAME := Alcatel 4032A
+TARGET_COMMON_NAME := alcatel 4032A
 BOARD_USE_MTK_LAYOUT := true
 BOARD_MTK_BOOT_LABEL := "/bootimg"
 BOARD_USE_NTFS_3G := false
 BRIGHTNESS_SYS_FILE := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 endif
 
+# TWRP
+ifeq ($(RECOVERY_VARIANT),twrp)
+TW_NO_EXFAT := true
+TWHAVE_SELINUX := true
+TW_THEME := portrait_mdpi
+TW_NO_EXFAT_FUSE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+TW_EXCLUDE_SUPERSU := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+DEVICE_RESOLUTION := 480x800
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+endif
