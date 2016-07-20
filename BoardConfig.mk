6829809289437efaad7232d9ac1690899cb325db
kirito96 <kirito96@users.noreply.github.com>
2016-07-20 18:54:57 -0400

    Revert "initial commit"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/alcatel/4032a/BoardConfigVendor.mk
+-include vendor/alcatel/4032A/BoardConfigVendor.mk
 
 # Architecture
 TARGET_ARCH := arm
@@ -14,29 +14,68 @@ TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := 4032a
+TARGET_BOOTLOADER_BOARD_NAME := 4032A
 
 BOARD_KERNEL_CMDLINE := 
-TARGET_PREBUILT_KERNEL := device/alcatel/4032a/prebuilt/kernel
-BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board 1429947554 --mtk 1
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
 
-# Partitions
-
+# Partition sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
-
-
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 545259520
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824
 BOARD_FLASH_BLOCK_SIZE := 512
 TARGET_USERIMAGES_USE_EXT4 := true
 
 BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/4032a/bootimg.mk
+BOARD_CUSTOM_MKBOOTIMG := $(HOST_OUT_EXECUTABLES)/mtkbootimg$(HOST_EXECUTABLE_SUFFIX)
+BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/4032A/bootimg.mk
+BOARD_MKBOOTIMG_ARGS := --mtk 1
 
-BOARD_CUSTOM_MKBOOTIMG := mtkbootimg
-TARGET_RECOVERY_FSTAB := device/alcatel/4032a/recovery/recovery.fstab
+TARGET_PREBUILT_KERNEL := device/alcatel/4032A/prebuilt/kernel
+TARGET_RECOVERY_INITRC := device/alcatel/4032A/recovery/init.rc
+TARGET_RECOVERY_FSTAB := device/alcatel/4032A/recovery/recovery.fstab
+
+DEVICE_SCREEN_WIDTH := 480
+DEVICE_SCREEN_HEIGHT := 800
+
+
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 RECOVERY_VARIANT := twrp
+#RECOVERY_VARIANT := philz
+#RECOVERY_VARIANT := cwm
+#RECOVERY_VARIANT := carliv
+
+ifneq ($(RECOVERY_VARIANT),carliv)
+TARGET_RECOVERY_FSTAB := device/alcatel/4032A/recovery/recovery.fstab
+endif
+
+
+# CARLIV
+ifeq ($(RECOVERY_VARIANT),carliv)
+BOARD_HAS_MTK := true
+BOARD_NEEDS_MTK_GETSIZE := true
+DEVICE_RESOLUTION := 480x800
+endif
+
+
+# PHILZ
+ifeq ($(RECOVERY_VARIANT),philz)
+TARGET_COMMON_NAME := alcatel 4032A
+BOARD_USE_MTK_LAYOUT := true
+BOARD_MTK_BOOT_LABEL := "/bootimg"
+BOARD_USE_NTFS_3G := false
+BRIGHTNESS_SYS_FILE := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+endif
 
 # TWRP
+ifeq ($(RECOVERY_VARIANT),twrp)
 TW_NO_EXFAT := true
 TWHAVE_SELINUX := true
 TW_THEME := portrait_mdpi
@@ -49,3 +88,4 @@ DEVICE_RESOLUTION := 480x800
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+endif
